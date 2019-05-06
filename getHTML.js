174767a3c1f233eab2bfbe7faf0d61a2391187cf
Justin <justin.s.kwok@gmail.com>
2019-05-06 18:23:20 +0000

    Added callback ~~!

---
+++ getHTML.js
@@ -0,0 +1,32 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+function getHTML (options, callback) {
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+    var output_data = "";
+
+    response.on('data', function (data) {
+      output_data+= data;
+      console.log('Chunk received. Length:', data.length);
+    });
+
+    response.on('end', function () {
+      callback(output_data);
+      console.log('Response stream complete');
+    });
+  });
+};
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
+
